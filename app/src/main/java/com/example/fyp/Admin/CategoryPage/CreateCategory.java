55f6a886c85d862b1ee43beaf85715e706a0a1ee
itsamekai <resayz4@ymail.com>
2021-05-26 20:06:31 +0800

    test commit - added create category in admin page

---
+++ app/src/main/java/com/example/fyp/Admin/CategoryPage/CreateCategory.java
@@ -0,0 +1,16 @@
+package com.example.fyp.Admin.CategoryPage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.fyp.R;
+
+public class CreateCategory extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_category);
+    }
+}
